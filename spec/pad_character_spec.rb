4d290e521a22f4fca9f2f6b935e84bd546bcd7cc
kashiwagi-rena <azisai07@gmail.com>
2023-02-27 16:05:14 +0900

    pad_character完成

--- spec/pad_character_spec.rb
+++ spec/pad_character_spec.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe PadCharacter do
-  it "has a version number" do
-    expect(PadCharacter::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-
-  it "入力した文字が全て🐾になる"
-    string = "あいうえお"
-    expect(string.to_pad).to eql("🐾🐾🐾🐾🐾")
+  it "入力した文字が全て🐾になる" do
+    str = "あいうえお"
+    expect(str.to_pad).to eql("🐾🐾🐾🐾🐾")
   end
 end
