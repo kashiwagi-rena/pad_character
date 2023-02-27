496e98721fa5a96748624818f2eaec1a2755fe22
kashiwagi-rena <azisai07@gmail.com>
2023-02-27 16:23:17 +0900

    format_pad_characterの改行をいい感じにした

--- lib/pad_character/format_pad_character.rb
+++ lib/pad_character/format_pad_character.rb
@@ -1,6 +1,6 @@
-  class String
-    def to_pad
-      word_count = self.size
-      pad_count = "\u{1F43E}" * word_count.to_i
-    end
+class String
+  def to_pad
+    word_count = self.size
+    pad_count = "\u{1F43E}" * word_count.to_i
   end
+end
